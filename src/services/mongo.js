b9ed9ff99d38f2c9079c1bcf69ba58aa0ef2ff09
sai1781 <96867450+sai1781@users.noreply.github.com>
2023-03-29 21:41:13 +0530

    Update mongo.js

--- src/services/mongo.js
+++ src/services/mongo.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 // Update below to match your own MongoDB connection string.
-const MONGO_URL = process.env.MONGO_URL;
+const MONGO_URL =  "mongodb+srv://sai:sP013Hr2fFGQdxeb@nasacluster.uolpi7m.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose.connection.once('open', () => {
   console.log('MongoDB connection ready!');
